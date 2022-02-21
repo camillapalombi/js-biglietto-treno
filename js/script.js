e240b18dd02144b108126abde1fbae1554668011
camillapalombi <camilla.palombi14@gmail.com>
2022-02-21 15:41:28 +0100

    PREZZO DEL BIGLIETTO SENZA SCONTI

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 /*BIGLIETTO TRENO*/
+
+let kilometers = prompt('Quanti km vuoi percorrere?');
+
+let age = prompt('Quanti anni hai?');
+
+const kmPrice = 0.21;
+
+const finalPrice = (kilometers * kmPrice);
+
+let element = document.getElementById('my_output');
+
+element.innerHTML = `Il prezzo del biglietto è di ${finalPrice} €`;
